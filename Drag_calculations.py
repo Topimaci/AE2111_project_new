66ff4080e9abbab51feba30c7bcc7dd7b7a0b04b
Benjamin Luneburg <benjaminluneburg@gmail.com>
2025-09-15 16:38:23 +0200

    updated Drag calculations

--- Drag_calculations.py
+++ Drag_calculations.py
@@ -1,8 +1,22 @@
 import math
 
-def C_D_function(C_D_0, C_L, AR, e):
-    C_D = C_D_0 + C_L**2/(math.pi*AR*e)
-    return C_D
+S_wet_over_S_w = 6
+C_f = 0.001
+psi = 
+phi = 
+AR = 
+
+
+
+def c_d0_function(S_wet_over_S_w, C_f):
+    c_d0 = (C_f * S_wet_over_S_w)
+    return c_d0
+
+
+
+def e_function(psi, phi, AR):
+    e = 1/(math.pi*AR*psi + phi)
+    return e
 
 
 def L_over_D_max_function(AR,e,C_D_0):  #### watch out what kind of C_L you are using!!
@@ -12,11 +26,14 @@ def L_over_D_max_function(AR,e,C_D_0):  #### watch out what kind of C_L you are 
 
     return  L_over_D_max, C_L_for_max_L_over_D, C_D_for_max_L_over_D
 
-def C_D_0_function(C_f_over_S_wet, C_f):
-    C_D_0 = (C_f * C_f_over_S_wet)
-    return C_D_0
+def c_d_function(C_D_0, C_L, AR, e):
+    C_D = C_D_0 + C_L**2/(math.pi*AR*e)
+    return C_D
 
-def e_function(psi, phi, AR):
-    e = 1/(math.pi*AR*psi + phi)
-    return e
 
+
+
+C_D_0_initial = c_d0_function(S_wet_over_S_w, C_f)
+e_initial = e_function(psi, phi, AR)
+
+L_over_D_max, C_L_for_max_L_over_D, C_D_for_max_L_over_D = L_over_D_max_function(AR,e_initial,C_D_0_initial)
