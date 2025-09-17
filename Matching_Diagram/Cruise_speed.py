423b17aa0eb0f7a40f911107d9a5873436edc806
Benjamin Luneburg <benjaminluneburg@gmail.com>
2025-09-17 15:13:35 +0200

    this is a test to show something

---
+++ Matching_Diagram/Cruise_speed.py
@@ -0,0 +1,17 @@
+import numpy
+import math
+
+#### thrust lapse = 0.24  beta = 0.95
+
+
+def cruise_speed(beta, thrust_lapse, Wing_loading, C_D_0, density, v_cr, AR, e):
+    T_over_W = numpy.array()
+    for i in Wing_loading:
+        x = (beta/thrust_lapse)*((C_D_0*0.5*density*(v_cr)**3)/(beta*i)+(beta*i)/(math.pi*AR*e*0.5*density*v_cr))**(-1)
+        numpy.append(T_over_W, x)
+
+    return T_over_W
+
+
+T_over_W_cruise_speed = cruise_speed()
+
