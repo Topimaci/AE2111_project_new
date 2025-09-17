dd2b3d4641a9bf0aefb49061c3ac6fc40efd2232
otomars-g <otomars.g@gmail.com>
2025-09-17 18:19:52 +0200

    Update Climb_rate.py

--- Matching_Diagram/Climb_rate.py
+++ Matching_Diagram/Climb_rate.py
@@ -18,35 +18,38 @@ Mach_number = []
 total_pressure = []
 lapse_rate = []
 
-def Climb_rate(wing_loading_input):
+def climb_rate(wing_loading_input):
+    global velocity_climb_rate, Mach_number, total_pressure, lapse_rate
     output = []
-    velocity_climb_rate = velocity_climb_rate(wing_loading_input)
-    Mach_number = Mach_number(velocity_climb_rate)
-    total_pressure = total_pressure(Mach_number)
-    lapse_rate = lapse_rate(total_pressure, Mach_number)
+    
+    velocity_climb_rate = velocity_climb_rate_func(wing_loading_input)
+    Mach_number = Mach_number_func(velocity_climb_rate)
+    total_pressure = total_pressure_func(Mach_number)
+    lapse_rate = lapse_rate_func(total_pressure, Mach_number)
 
     for i in range(len(wing_loading_input)):
-        output = np.append((mass_fraction_climb/lapse_rate[i]) * (climb_rate_requirement/velocity_climb_rate[i] + (2 * m.sqrt(C_d0/(m.pi() * AR * e)))))
+        output = np.append(output, (mass_fraction_climb/lapse_rate[i]) * (climb_rate_requirement/velocity_climb_rate[i] + (2 * m.sqrt(C_d0/(m.pi * AR * e)))))
+    return output
 
-def velocity_climb_rate(wing_loading_input):
+def velocity_climb_rate_func(wing_loading_input):
     output = []
     for value in wing_loading_input:
         output = np.append(output, m.sqrt(value**2/(rho_ISO*0.728)))
     return output
         
-def Mach_number(velocity_climb_rate_output):
+def Mach_number_func(velocity_climb_rate_output):
     output = []
     for value in velocity_climb_rate_output:
         output = np.append(output, value/m.sqrt(T_ISO*1.4*287))
     return output
 
-def total_pressure(Mach_number_output):
+def total_pressure_func(Mach_number_output):
     output = []
     for i, value in enumerate(Mach_number_output):
         output = np.append(output, 1 + 0.2 * Mach_number_output[i]**2)
     return output
 
-def lapse_rate(total_pressure_output, Mach_number_output):
+def lapse_rate_func(total_pressure_output, Mach_number_output):
     output = []
     for i in range(len(total_pressure_output)):
         output = np.append(output, total_pressure_output[i] * (1 - (0.43 + 0.014 * B) * m.sqrt(Mach_number_output[i])))
