fixed gradient

fixed_values.py
@@ -43,7 +43,7 @@ oswald_efficiency = 0.887
 
 #________Wing Loading______
 
-wing_loading = np.arange(0,9100,100)
+wing_loading = np.arange(100,9100,100)
 wing_loading_cs = np.arange(1200, 9100, 100)
 
 
@@ -63,23 +63,23 @@ cg_121b = 2.4
 cg_121c = 1.2
 cg_121d = 2.1
 
-C_d0_119 = 0.0707175
-C_d0_121a = 0.0447175
-C_d0_121b = 0.0347175
-C_d0_121c = 0.0087175
-C_d0_121d = 0.0607175
+C_d0_119 = 0.0824
+C_d0_121a = 0.0590
+C_d0_121b = 0.0390
+C_d0_121c = 0.0234
+C_d0_121d = 0.0624
 
-e_119 = 0.901657174
-e_121a = 0.849657174
-e_121b = 0.849657174
-e_121c = 0.797657174
-e_121d = 0.901657174
+e_119 = 0.9336
+e_121a = 0.8868
+e_121b = 0.8868
+e_121c = 0.8556
+e_121d = 0.9336
 
 mass_fraction_119 = 1
 mass_fraction_121a = 1
 mass_fraction_121b = 1
 mass_fraction_121c = 1
-mass_fraction_121d = 0.69
+mass_fraction_121d = 0.8
 
 C_l_at_max_climb_gradient_119 = m.sqrt(C_d0_119 * m.pi * AR * e_119)
 C_l_at_max_climb_gradient_121a = m.sqrt(C_d0_121a * m.pi * AR * e_121a)
@@ -87,8 +87,8 @@ C_l_at_max_climb_gradient_121b = m.sqrt(C_d0_121b * m.pi * AR * e_121b)
 C_l_at_max_climb_gradient_121c = m.sqrt(C_d0_121c * m.pi * AR * e_121c)
 C_l_at_max_climb_gradient_121d = m.sqrt(C_d0_121d * m.pi * AR * e_121d)
 
-C_d_at_max_climb_gradient_119 = 2 * C_l_at_max_climb_gradient_119 
-C_d_at_max_climb_gradient_121a = 2 * C_l_at_max_climb_gradient_121a
-C_d_at_max_climb_gradient_121b = 2 * C_l_at_max_climb_gradient_121b
-C_d_at_max_climb_gradient_121c = 2 * C_l_at_max_climb_gradient_121c
-C_d_at_max_climb_gradient_121d = 2 * C_l_at_max_climb_gradient_121d
+C_d_at_max_climb_gradient_119 = C_d0_119+C_l_at_max_climb_gradient_119**2/(e_119*AR*m.pi)
+C_d_at_max_climb_gradient_121a = C_d0_121a+C_l_at_max_climb_gradient_121a**2/(e_121a*AR*m.pi)
+C_d_at_max_climb_gradient_121b = C_d0_121b+C_l_at_max_climb_gradient_121b**2/(e_121b*AR*m.pi)
+C_d_at_max_climb_gradient_121c = C_d0_121c+C_l_at_max_climb_gradient_121c**2/(e_121c*AR*m.pi)
+C_d_at_max_climb_gradient_121d = C_d0_121d+C_l_at_max_climb_gradient_121d**2/(e_121d*AR*m.pi)