a8e72e877a5e8f328f437101e52886e64a57529b
Bence <btomopulosz@tudelft.nl>
2025-09-22 15:54:02 +0200

    I have no idea what happenned

--- Matching_Diagram/Take_off_distance.py
+++ Matching_Diagram/Take_off_distance.py
@@ -4,14 +4,14 @@ import math
 def find_V_2(wing_loading, density_take_off, C_L_take_off):
     V_2 = []
     for i in wing_loading: 
-        x = math.sqrt(2 * i / density_take_off / C_L_take_off)
+        x = numpy.sqrt(2 * i / density_take_off / C_L_take_off)
         V_2.append(x)
     return V_2
 
 def find_Mach(V_2, temperature_take_off):
     Mach_take = []
     for j in V_2: 
-        y = j / math.sqrt(1.4 * 287 * temperature_take_off)
+        y = j / numpy.sqrt(1.4 * 287 * temperature_take_off)
         Mach_take.append(y)
     return Mach_take
 
@@ -26,13 +26,18 @@ def find_theta_delta(temperature_take_off, Mach_take):
     return theta, delta
 
 def find_alpha_t(delta, Mach_take, bypass_ratio):
-    alpha_t = [l * (1 - (0.43 + 0.014 * bypass_ratio) * math.sqrt(m)) for l, m in zip(delta, Mach_take)]
-    return alpha_t
+
+    alpha_t_list = []
+
+    for l, m in zip(delta, Mach_take):
+        val = l * (1 - (0.43 + 0.014 * bypass_ratio) * numpy.sqrt(m))
+        alpha_t_list.append(val)
+    return alpha_t_list
 
 def take_off_distance(alpha_t, wing_loading, take_off_field_length, density_take_off, oswald_efficiency, aspect_ratio):
-    T_over_W = numpy.array()
+    T_over_W = numpy.array([])
     for wl, at in zip(wing_loading, alpha_t):
-        q = 1 / at * (1.15 * math.sqrt(2 * wl / (take_off_distance * 0.85 * density_take_off * 9.80065 * math.pi * oswald_efficiency * aspect_ratio)) + 4 * 11 * 2 / take_off_distance)
-        numpy.append(wl, q)
+        q = 1 / at * (1.15 * math.sqrt(2 * wl / (take_off_field_length * 0.85 * density_take_off * 9.80065 * math.pi * oswald_efficiency * aspect_ratio)) + 4 * 11 * 2 / take_off_field_length)
+        T_over_W = numpy.append(T_over_W, q)
     return T_over_W
     
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Planform_DESIGN1 import *
+import Planform_DESIGN1 as pd
 from Drag_calculations import *
 import Matching_Diagram.Minimum_speed as ms
 import Matching_Diagram.Climb_rate as cr
@@ -21,9 +21,9 @@ psi = 0.0075
 phi = 0.97
 
 
-sweep, taper, b , c_r, c_t, c_MAC, dihedral, sweep_LE = calculate_geometric_parameters_wing(S_w, AR, M_cr)
+sweep, taper, b , c_r, c_t, c_MAC, dihedral, sweep_LE = pd.calculate_geometric_parameters_wing(S_w, AR, M_cr)
 
-c_d0 = calculate_aerodynamic_performance(thickness_to_chord)
+c_d0 = pd.calculate_aerodynamic_performance(thickness_to_chord)
 
 c_d_0_initial = c_d0_function(S_wet_over_S_w, C_f)
 
@@ -31,22 +31,22 @@ e_initial = e_function(psi, phi, AR)
 
 L_over_D_max, C_L_for_max_L_over_D, C_D_for_max_L_over_D = L_over_D_max_function(AR,e_initial,c_d_0_initial)
 
-y_spanwise, xlemac, lengthMAC = calculate_MAC_position(b, c_r, c_t, sweep)
+y_spanwise, xlemac, lengthMAC = pd.calculate_MAC_position(b, c_r, c_t, sweep)
 
 #_________Wing Loading Calculations______________________________________________________________________________________________________
 loads_minimum_speed = ms.Minimum_speed_function(fv.wing_loading)
-loads_landing_field_length = lfl.landing_field_length(fv.mass_fraction, 950, 1.225, fv.C_L_max_landing)
-loads_cruise_speed = cs.cruise_speed(fv.beta,fv.thrust_lapse,fv.wing_loading,fv.C_d0, 1.225, fv.v_cr, fv.AR, fv.e_f)
+loads_landing_field_length = lfl.landing_field_length(fv.mass_fraction_landing, 950, 1.225, fv.C_L_max_landing)
+loads_cruise_speed = cs.cruise_speed(fv.beta,fv.thrust_lapse,fv.wing_loading_cs,fv.C_d0, 1.225, fv.v_cr, fv.AR, fv.e_f)
 loads_climb_rate = cr.climb_rate(fv.wing_loading)
-loads_climb_grad_119 = cg.climb_grad(fv.wing_loading, massfraction, fv.cg_119, fv.C_d0_119, fv.e_119, fv.AR, 1.225, nemtom, fv.B)
-loads_climb_grad_121a = cg.climb_grad(fv.wing_loading, massfraction, fv.cg_121a,fv.C_d0_121a, fv.e_121a, fv.AR, 1.225, nemtom, fv.B)
-loads_climb_grad_121b = cg.climb_grad(fv.wing_loading, massfraction, fv.cg_121b, fv.C_d0_121b, fv.e_121b, fv.AR, 1.225, nemtom, fv.B)
-loads_climb_grad_121c = cg.climb_grad(fv.wing_loading, massfraction, fv.cg_121c, fv.C_d0_121c, fv.e_121c, fv.AR, 1.225, nemtom, fv.B)
-loads_climb_grad_121d = cg.climb_grad(fv.wing_loading, massfraction, fv.cg_121d, fv.C_d0_121d, fv.e_121d, fv.AR, 1.225, nemtom, fv.B)
+loads_climb_grad_119 = cg.climb_grad(fv.wing_loading, fv.mass_fraction_119, fv.cg_119, fv.C_d0_119, fv.e_119, fv.AR, 1.225, fv.C_l_at_max_climb_gradient_119, fv.B)
+loads_climb_grad_121a = cg.climb_grad(fv.wing_loading, fv.mass_fraction_121a, fv.cg_121a,fv.C_d0_121a, fv.e_121a, fv.AR, 1.225, fv.C_l_at_max_climb_gradient_121a, fv.B)
+loads_climb_grad_121b = cg.climb_grad(fv.wing_loading, fv.mass_fraction_121b, fv.cg_121b, fv.C_d0_121b, fv.e_121b, fv.AR, 1.225, fv.C_l_at_max_climb_gradient_121b, fv.B)
+loads_climb_grad_121c = cg.climb_grad(fv.wing_loading, fv.mass_fraction_121c, fv.cg_121c, fv.C_d0_121c, fv.e_121c, fv.AR, 1.225, fv.C_l_at_max_climb_gradient_121c, fv.B)
+loads_climb_grad_121d = cg.climb_grad(fv.wing_loading, fv.mass_fraction_121d, fv.cg_121d, fv.C_d0_121d, fv.e_121d, fv.AR, 1.225, fv.C_l_at_max_climb_gradient_121d, fv.B)
 V2 = td.find_V_2(fv.wing_loading, fv.density_takeoff, fv.CL_takeoff)
 Mach = td.find_Mach(V2, fv.temp_takeoff)
-theta_delta = td.find_theta_delta(fv.temp_takeoff, Mach)
-alpha = td.find_alpha_t(theta_delta, Mach, fv.B)
+theta, delta = td.find_theta_delta(fv.temp_takeoff, Mach)
+alpha = td.find_alpha_t(delta, Mach, fv.B)
 loads_to_field = td.take_off_distance(alpha, fv.wing_loading, fv.takeoff_field, fv.density_takeoff, fv.oswald_efficiency, fv.AR)
 
 print(sweep, taper, b , c_r, c_t, c_MAC, dihedral, sweep_LE)
@@ -56,9 +56,20 @@ print(e_initial)
 print(L_over_D_max, C_L_for_max_L_over_D, C_D_for_max_L_over_D )
 print("THIS ONE", y_spanwise, xlemac, lengthMAC)
 
+print("loads_minimum_speed:", loads_minimum_speed)
+print("loads_landing_field_length:", loads_landing_field_length)
+print("loads_cruise_speed:", loads_cruise_speed)
+print("loads_climb_rate:", loads_climb_rate)
+print("loads_climb_grad_119:", loads_climb_grad_119)
+print("loads_climb_grad_121a:", loads_climb_grad_121a)
+print("loads_climb_grad_121b:", loads_climb_grad_121b)
+print("loads_climb_grad_121c:", loads_climb_grad_121c)
+print("loads_climb_grad_121d:", loads_climb_grad_121d)
+print("loads_to_field:", len(loads_to_field))
+
 #____Plots____
 
-plotname = f"Matching_Diagram_iteration_{i}.png"
+plotname = f"Matching_Diagram_iteration.png"
 
 fig, dx = plt.subplots()
 dx.plot(fv.wing_loading_cs, loads_cruise_speed, label="Cruise speed")
@@ -69,7 +80,7 @@ dx.plot(fv.wing_loading, loads_climb_grad_121c, label="Climb gradient CS25.121c"
 dx.plot(fv.wing_loading, loads_climb_grad_121d, label="Climb gradient CS25.121d")
 dx.plot(fv.wing_loading, loads_to_field, label="Take-off field length")
 dx.axvline(loads_minimum_speed[0], color = "gold", label = "Minimum speed")
-dx.axvline(loads_landing_field_length[0], color = "black", label = "Landing field length")
+dx.axvline(loads_landing_field_length, color = "black", label = "Landing field length")
 dx.set_xlim([0, 9000])
 dx.set_ylim([0.1, 0.6])
 dx.set_xlabel("W/S - [N/m2]")
@@ -77,4 +88,6 @@ dx.set_ylabel("T/W - [N/N]")
 dx.set_title("Matching Diagram")
 legend = dx.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=3)
 plt.savefig(plotname, bbox_extra_artists=(legend,), bbox_inches='tight')
+
+#_____iterative process_____________________________________________________________________________
 
