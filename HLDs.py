190bb237b32b248f4072dd06e8ca12811b86199f
Sarah Brandlehner <sa.brandlehner@gmail.com>
2025-10-02 12:37:23 +0200

    update HLD calculation

--- HLDs.py
+++ HLDs.py
@@ -13,26 +13,29 @@ c_f_c_LE = 0.1
 sweep, taper, b, c_root, c_tip, c_MAC, dihedral, sweep_LE = Pl.calculate_geometric_parameters_wing(30.46, fv.AR, 0.68, 24.5)
 
 sweep_LE = math.radians(sweep_LE)
+half_b = b / 2 
 
 # trailing edge reference area
 sweep_TE = math.atan(math.tan(sweep_LE) + (c_tip - c_root) / (b / 2) )
 
-x_1 = (end_pos_span_TE - start_pos_span_TE) * b / 2
-area_1 = (x_1) ** 2 * math.tan(sweep_LE) / 2 
-area_2 = (x_1) * (c_root - x_1 * math.tan(sweep_LE))
-area_3 = x_1 ** 2 * math.tan(sweep_TE) / 2 
+length_TE = (end_pos_span_TE - start_pos_span_TE) * b / 2
+area_1 = (length_TE) ** 2 * math.tan(sweep_LE) / 2 
+area_2 = (length_TE) * (c_root - length_TE * math.tan(sweep_LE))
+area_3 = length_TE ** 2 * math.tan(sweep_TE) / 2 
 
 reference_area_TE = (area_1 + area_2 + area_3) * 2
 
 # leading edge reference area
-x_2 = end_pos_span_LE - start_pos_span_LE  
-half_b = b / 2 
-Area1 = x_2 * half_b * math.sin(sweep_LE) * x_2 * half_b / 2  # top triangle
-Area2 = x_2 * half_b * (c_root - (x_2 + start_pos_span_LE) * half_b * math.sin(sweep_LE))  # big rectangle
-Area3 = start_pos_span_LE * half_b * math.sin(sweep_TE) * x_2 * half_b  # small rectangle
-Area4 = x_2 * half_b * math.sin(sweep_TE) * x_2 * half_b / 2  # bottom triangle
+Datum = c_tip + half_b * math.tan(sweep_LE)
+y_1 = start_pos_span_LE * half_b * math.tan(sweep_LE)
+x_1 = end_pos_span_LE * half_b * math.tan(sweep_LE)
+x_2 = end_pos_span_LE * half_b * math.tan(sweep_TE)
+y_2 = start_pos_span_LE * half_b * math.tan(sweep_TE)
 
-reference_area_LE = (Area1 + Area2 + Area3 + Area4) * 2
+c_1 = Datum - x_1 - x_2 
+c_2 = Datum - y_1 - y_2 
+
+reference_area_LE = (c_1 + c_2) / 2 * (end_pos_span_LE - start_pos_span_LE) * half_b
 
 
 # C_L_max calculation trailing edge fowler flap
@@ -56,3 +59,5 @@ delta_c_lmax_LE = 0.4 * c_prime_LE / c_avg_LE
 DELTA_c_LMAX_LE = 0.9 * delta_c_lmax_LE * reference_area_LE / wing_area * math.cos(sweep_LE)
 
 print(DELTA_c_LMAX_LE + DELTA_c_LMAX_TE)
+
+print(reference_area_LE)
