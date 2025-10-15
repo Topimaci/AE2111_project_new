9eec86a93a11582a70a418fc66818f639a18d186
Otomars Gulbis <otomars.g@gmail.com>
2025-10-15 14:38:03 +0200

    more engine values

--- functions/Engine_types.py
+++ functions/Engine_types.py
@@ -1,11 +1,12 @@
-## model, thrust[N], mass[kg], length, diameter, CG
+# Format: ["Engine name", T_per_engine_N, Weight_per_engine_kg, Wtot_plus_nacelle, Nacelle_diameter_m, Nacelle_length_m, CG_from_front_m, B_ratio]
 engines = [
-    ["Honeywell HTF7500E", 33486, 687, 2.282, 1.116],
-    ["Honeywell HTF7350 turbofans", 32574, 696, 2.281, 1.156],
-    ["Honeywell HTF7700L", 34100, 696, 2.282, 1.156],
-    ["Pratt & Whitney Canada PW305A", 23200, 452.5, 1.892, 1.147],
-    ["Honeywell HTF7250G", 33900, 696, 2.282, 1,156]
+    ["Honeywell HTF7500E", 33486, 687, 916, 1.116, 2.282, 0.9128, 4.2],
+    ["Honeywell HTF7350 turbofans", 32574, 696, 928, 1.156, 2.281, 0.9124, 4.2],
+    ["Honeywell HTF7700L", 34100, 696, 928, 1.156, 2.282, 0.9128, 4.2],
+    ["Pratt & Whitney Canada PW305A", 23200, 452.5, 603.3333333, 1.147, 2.06, 0.824, 4.5]
+    ["Honeywell HTF7250G", 33900, 696, 928, 1.156, 2.281, 0.9124, 4.2]
 ]
+
 
 engines_sorted = []
 
