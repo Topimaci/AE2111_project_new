941be976ff2d2940844a1b9809cd66c1eb019d43
Otomars Gulbis <otomars.g@gmail.com>
2025-11-10 15:54:25 +0100

    style fixes

--- functions/Engine_types.py
+++ functions/Engine_types.py
@@ -6,11 +6,11 @@ def engine_required(thrust_required):
     # Format: ["Engine name", T_per_engine_N, Weight_per_engine_kg, Wtot_plus_nacelle, Nacelle_diameter_m, 
     #           Nacelle_length_m, CG_from_front_m, B_ratio, Specific_fuel_consumption_cruise_kg/N/s]
     engines = [
-    ["Honeywell HTF7500E",          33486, 687, 916, 1.116, 2.282, 0.9128, 4.2, 0.0000182],
-    ["Honeywell HTF7350 turbofans", 32574, 696, 928, 1.156, 2.281, 0.9124, 4.2, 0.0000182],
-    ["Honeywell HTF7700L",          34100, 696, 928, 1.156, 2.282, 0.9128, 4.2, 0.0000122],
-    ["Pratt & Whitney Canada PW305A",23200, 452.5, 603.3333, 1.147, 2.06, 0.824, 4.5, 0.000019417],
-    ["Honeywell HTF7250G",          33900, 696, 928, 1.156, 2.281, 0.9124, 4.2, 0.0000182]
+    ["Honeywell HTF7500E",            33486, 687,   916,     1.116, 2.282, 0.9128, 4.2, 0.0000182],
+    ["Honeywell HTF7350 turbofans",   32574, 696,   928,     1.156, 2.281, 0.9124, 4.2, 0.0000182],
+    ["Honeywell HTF7700L",            34100, 696,   928,     1.156, 2.282, 0.9128, 4.2, 0.0000122],
+    ["Pratt & Whitney Canada PW305A", 23200, 452.5, 603.333, 1.147, 2.06,  0.824,  4.5, 0.000019417],
+    ["Honeywell HTF7250G",            33900, 696,   928,     1.156, 2.281, 0.9124, 4.2, 0.0000182]
 ]   
 
 
