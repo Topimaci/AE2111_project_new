e0ea98adc2d5303e19f60b223453688220697304
Jesse Misdorp <j.misdorp@student.tudelft.nl>
2025-11-19 14:31:27 +0100

    importing shear load line

--- WP4/Moment_Diagram.py
+++ WP4/Moment_Diagram.py
@@ -1,1 +1,10 @@
-# test
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from XFLR import y_span, chord, Ai, Cl, ICd, Cm
+
+
+from scipy import integrate, interpolate
+
+from TorqueDist import L_prime
+
