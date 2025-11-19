a0352a98a93c043d2862abc1608a86d154fcd8c1
Jesse Misdorp <j.misdorp@student.tudelft.nl>
2025-11-19 15:57:53 +0100

    Importing and calc q_func

--- WP4/Moment_Diagram.py
+++ WP4/Moment_Diagram.py
@@ -8,21 +8,24 @@ from scipy import integrate, interpolate
 
 
 from TorqueDist import compute_lift_line_load
+from TorqueDist import build_q_d_t_functions
+from TorqueDist import N_prime
+from TorqueDist import M_prime
 
 #Variables
 b_half = 9.7925 #half of the wing span in m
 V_inf = 10.0  # Freestream velocity in m/s
 rho   = 1.225 # Air density in kg/m^3
 
+#determining q_func
 
-L_prime = compute_lift_line_load(chord, Cl, V_inf, rho)
-
+y, q_func, d_func, t_func = build_q_d_t_functions(y_span, N_prime, M_prime)
 
 # Integrating load line -> shear load line S(y)
 
-S, error_S = sp.integrate.quad(L_prime, 0, b_half)
+#S, error_S = sp.integrate.quad(q_func, 0, b_half)
 
 # Integrating S(y) -> M(y)      negative sign???
-M, error_M = sp.integrate.quad(S, 0, b_half)
+#M, error_M = sp.integrate.quad(S, 0, b_half)
 
-print(M)
+#print(S)
