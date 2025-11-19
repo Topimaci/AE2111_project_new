62807b187e3214f862a7d07c77d9298c4c979243
Oriol Ventosa <uri.ventosa@gmail.com>
2025-11-19 13:58:49 +0100

    added some comments

--- WP4/XFLR.py
+++ WP4/XFLR.py
@@ -7,7 +7,7 @@ with warnings.catch_warnings():
     data = np.genfromtxt("angleofattack0.txt", skip_header=21, skip_footer=60, invalid_raise=False)
 
 # In the XFLR file there are many columns that aren't of interest, for this reason only those needed are extracted from the data matrix found above. Keeping the data matrix allows 
-# 
+# extracting other values if they are ever needed. first value ":" means all values in column (all rows), while second means "column number" (column position in the XFLR txt file).
 y_span = data[:, 0]
 chord  = data[:, 1]
 Ai     = data[:, 2]
@@ -15,7 +15,7 @@ Cl     = data[:, 3]
 ICd    = data[:, 5]
 Cm     = data[:, 7]
 
-# Quick check
+#check
 print("y positions:", y_span[:])
 print("Chord lengths:", chord[:])
 print("Lift coefficients:", Cl[:])
