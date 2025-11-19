79312943b5bc25674963de37ff567e6d877f689a
Oriol Ventosa <uri.ventosa@gmail.com>
2025-11-19 13:56:18 +0100

    added/ corrected the XFLR extraction file.

--- WP4/XFLR.py
+++ WP4/XFLR.py
@@ -1,9 +1,13 @@
 import numpy as np
+import warnings
 
-# Load all numeric data after 20 header lines
-data = np.genfromtxt("angleofattack0.txt", skip_header=21, skip_footer=60, invalid_raise=False)
+# Load all numeric data after 20 header lines (suppress warnings about missing columns)
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+    data = np.genfromtxt("angleofattack0.txt", skip_header=21, skip_footer=60, invalid_raise=False)
 
-# Assign only the columns you care about to separate variables
+# In the XFLR file there are many columns that aren't of interest, for this reason only those needed are extracted from the data matrix found above. Keeping the data matrix allows 
+# 
 y_span = data[:, 0]
 chord  = data[:, 1]
 Ai     = data[:, 2]
@@ -17,4 +21,3 @@ print("Chord lengths:", chord[:])
 print("Lift coefficients:", Cl[:])
 print("Induced drag:", ICd[:])
 print("Quarter-chord moment:", Cm[:])
-
