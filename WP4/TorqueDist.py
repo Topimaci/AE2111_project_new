933990f8dc7df28022ce37836e9d0a9d4b0b6995
Caia Wijnschenk <caiawijnschenk@gmail.com>
2025-11-19 11:09:13 +0100

    Torque distr. addition

--- WP4/TorqueDist.py
+++ WP4/TorqueDist.py
@@ -1,19 +1,64 @@
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
 
 
-def load_distribution(): #q(x)
-    d_1 = distance to shear center
+# Variables
 
-    # Placeholder function for loading distribution data)
+V_inf = 10.0  # Freestream velocity in m/s
+rho   = 1.225 # Air density in kg/m^3
+aoa = 0.0   # Angle of attack in degrees
 
-def torsion_moment(): #t(x)
-    # Placeholder function for torsion moment calculations
+# Importing data and computing aerodynamic line load
 
-def distance_to_flexural_neutral_axis():: #d(x)
-    # Placeholder function for distance to flexural neutral axis calculations
+filename = 'angleofattack0.txt'
 
-def applied_loads():
-    d_2 = distance to shear center
-    # Placeholder function for applied loads calculations
+# 1. loading data
 
+def load_xflr_data(filename: str):
+    data = np.loadtxt(filename, skiprows=21)
+
+    y_span = data[:, 0]
+    chord  = data[:, 1]
+    Ai     = data[:, 2]
+    Cl     = data[:, 3]
+    ICd    = data[:, 5]
+    Cm     = data[:, 7]
+
+    return y_span, chord, Ai, Cl, ICd, Cm
+
+
+def compute_lift_line_load(chord: np.ndarray,
+                           Cl: np.ndarray,
+                           V_inf: float = 10.0,
+                           rho: float = 1.225) -> np.ndarray:
+    """
+    L'(y) = 0.5 * rho * V^2 * Cl(y) * c(y)
+    """
+    q_inf = 0.5 * rho * V_inf**2
+    L_prime = q_inf * chord * Cl
+    return L_prime
+
+def compute_drag_line_load(chord: np.ndarray,
+                           ICd: np.ndarray,
+                           V_inf: float = 10.0,
+                           rho: float = 1.225) -> np.ndarray:
+    """
+    D'(y) = 0.5 * rho * V^2 * Cd(y) * c(y)
+    """
+    q_inf = 0.5 * rho * V_inf**2
+    D_prime = q_inf * chord * ICd
+    return D_prime
+
+def compute_normal_force_distribution(L_prime: np.ndarray,
+                                      aoa: float,
+                                      V_inf: float = 10.0,
+                                      rho: float = 1.225) -> np.ndarray:
+    """
+    N'(y) = L'(y) * cos(aoa) + D'(y) * sin(aoa)
+    """
+    aoa_rad = np.radians(aoa)
+    N_prime = L_prime * np.cos(aoa_rad) + D_prime * np.sin(aoa_rad)
+    return N_prime
+                                      
+                                      
