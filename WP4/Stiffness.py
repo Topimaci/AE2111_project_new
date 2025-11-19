ec79433f513ef9a4ab5f0ccc8d7082803dec4325
Otomars Gulbis <otomars.g@gmail.com>
2025-11-19 23:57:13 +0100

    Spar code fixes in stiffness

--- WP4/Stiffness.py
+++ WP4/Stiffness.py
@@ -4,6 +4,131 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
+##______Spar length based on airfoil and y-position________________________________________________________
+Airfoil_coordinates = []
+spar_location_fraction1 = 0.1  # Spar location as a fraction of chord length, front spar
+spar_location_fraction2 = 0.6  # Spar location as a fraction of chord length, rear spar
+
+
+## Read airfoil coordinates from file
+with open("WP4/NACA64714 a=0.0.dat", "r") as file:
+    for line in file:
+        ## Skip blank lines or header text
+        line = line.strip()
+        if not line or not line[0].isdigit():
+            continue
+
+        x, y = map(float, line.split())
+        Airfoil_coordinates.append([x, y])
+
+
+## For a single spar calculate the top and bottom closest points from the airfoil map
+def spar_position(Airfoil_coordinates, spar_location_fraction):
+    closest_coordinates_1 = [0, 0]
+    closest_coordinates_2 = [0, 0]
+    closest_coordinates_3 = [0, 0]
+    closest_coordinates_4 = [0, 0]
+    previous_coords = [0, 0]
+    one_found = True
+
+    ## Find the boundary coordinates for the spar location
+    for coords in Airfoil_coordinates:
+        ## Top spar coordinates
+        if spar_location_fraction > coords[0] and one_found:
+            closest_coordinates_2 = coords
+            closest_coordinates_1 = previous_coords
+            one_found = False
+        ## Bottom spar coordinates
+        if spar_location_fraction < coords[0] and not one_found:
+            closest_coordinates_4 = coords
+            closest_coordinates_3 = previous_coords
+            break
+        previous_coords = coords
+    return(closest_coordinates_1, closest_coordinates_2, closest_coordinates_3, closest_coordinates_4)
+
+## Find the top stringer y coordinates based on the front and rear spar, they are horizontal
+def top_stringer_y_coord(spar1_coor1, spar1_coor2, spar2_coor1, spar2_coor2, spar_location_fraction1):
+    ## Find the bottom of the top stringer
+    y_top_spar1 = max(spar1_coor1[1], spar1_coor2[1])
+    y_top_spar2 = max(spar2_coor1[1], spar2_coor2[1])
+    top_y = min(y_top_spar1, y_top_spar2)
+    ## Find the top of the bottom stringer
+    y_bot_spar1 = min(spar1_coor1[1], spar1_coor2[1])
+    y_bot_spar2 = min(spar2_coor1[1], spar2_coor2[1])
+    bot_y = max(y_bot_spar1, y_bot_spar2)
+    
+    if top_y - bot_y <= 0: ## If there is no overlap, take the average
+        return 1/2 * (y_top_spar1 + y_top_spar2)
+    else: ## If there is overlap, find the y coordinate at the spar chord-wise location via a line between two of the bounding points
+        a = (spar1_coor2[1] - spar1_coor1[1])/(spar1_coor2[0] - spar1_coor1[0])
+        b = spar1_coor1[1] - a * spar1_coor1[0]
+        y_at_spar_location = a * spar_location_fraction1 + b
+        return y_at_spar_location
+
+## Find the bottom stringer y coordinates based on the front and rear spar
+def bot_stringer_y_coords(spar1_coor3, spar1_coor4, spar2_coor3, spar2_coor4, spar_location_fraction1, spar_location_fraction2):
+    a1 = (spar1_coor4[1] - spar1_coor3[1])/(spar1_coor4[0] - spar1_coor3[0])
+    b1 = spar1_coor3[1] - a1 * spar1_coor3[0]
+    y1_at_spar_location = a1 * spar_location_fraction1 + b1
+
+    a2 = (spar2_coor4[1] - spar2_coor3[1])/(spar2_coor4[0] - spar2_coor3[0])
+    b2 = spar2_coor3[1] - a2 * spar2_coor3[0]
+    y2_at_spar_location = a2 * spar_location_fraction2 + b2
+
+    return y1_at_spar_location, y2_at_spar_location
+
+## Find the spar length fraction based on the box coordinates
+def spar_length_fraction(box_coordinates, spar_location_fraction):
+    a = (box_coordinates[3][1] - box_coordinates[2][1])/(box_coordinates[3][0] - box_coordinates[2][0])
+    b = box_coordinates[2][1] - a * box_coordinates[2][0]
+    y_at_spar_location = a * spar_location_fraction + b
+    fraction = box_coordinates[0][1] - y_at_spar_location
+    return fraction
+
+## Final spar length calculation function
+def spar_length(spar_location_fraction, y_coordinate, root_chord, tip_chord, span):
+    spar1_coor1, spar1_coor2, spar1_coor3, spar1_coor4 = spar_position(Airfoil_coordinates, spar_location_fraction1) ## Front spar, top right, top left, bottom left, bottom right
+    spar2_coor1, spar2_coor2, spar2_coor3, spar2_coor4 = spar_position(Airfoil_coordinates, spar_location_fraction2) ## Rear spar, top right, top left, bottom left, bottom right
+    y_at_top_stringer = top_stringer_y_coord(spar1_coor1, spar1_coor2, spar2_coor1, spar2_coor2, spar_location_fraction1)
+    y1_at_bot_stringer, y2_at_bot_stringer = bot_stringer_y_coords(spar1_coor3, spar1_coor4, spar2_coor3, spar2_coor4, spar_location_fraction1, spar_location_fraction2)
+    box_coordinates = [[spar_location_fraction2, y_at_top_stringer], [spar_location_fraction1, y_at_top_stringer],
+                    [spar_location_fraction1, y1_at_bot_stringer], [spar_location_fraction2, y2_at_bot_stringer]] ## [top right, top left, bottom left, bottom right]
+    length_fraction = spar_length_fraction(box_coordinates, spar_location_fraction)
+    a = (tip_chord - root_chord)/(span/2)
+    chord_length_at_y = root_chord + a * y_coordinate
+    spar_length = length_fraction * chord_length_at_y
+
+    ## plot the airfoil and torsion box
+    '''
+    xpoints = []
+    ypoints = []
+    for coords in Airfoil_coordinates:
+        xpoints.append(coords[0])
+        ypoints.append(coords[1])
+
+    xbox = []
+    ybox = []
+    for coords in box_coordinates:
+        xbox.append(coords[0])
+        ybox.append(coords[1])
+
+    plt.axis('equal')
+    plt.plot(xpoints, ypoints)
+    #plt.plot([spar1_coor1[0], spar1_coor2[0], spar1_coor3[0], spar1_coor4[0]],
+    #        [spar1_coor1[1], spar1_coor2[1], spar1_coor3[1], spar1_coor4[1]], 'o', color='red')
+    #plt.plot([spar2_coor1[0], spar2_coor2[0], spar2_coor3[0], spar2_coor4[0]],
+    #        [spar2_coor1[1], spar2_coor2[1], spar2_coor3[1], spar2_coor4[1]], 'o', color='red')
+    plt.plot(xbox + [xbox[0]], ybox + [ybox[0]], color='green')
+    plt.show()
+    '''
+
+    return spar_length
+
+print(spar_length(0, 0.101, 2.874, 1.043, 19.585)) ## Spar_location_fraction, y_coordinate, root_chord, tip_chord, span
+
+
+
+##______What part is this________________________________________________________
 b = 19.585    # hard coded for now, should probably be pulled from somewhere in the code later on
 max_displ = 0.15 * b
 max_tip_rotat_deg = 10   # in degrees
@@ -83,4 +208,3 @@ dth_dy = T / (G * J)
 estimate_dv, error_dv = sp.integrate.quad(d2v_dy2, 0, b)
 estimate_v, error_v = sp.integrate.quad(estimate_dv, 0, b)
 estimate_th, error_th = sp.integrate.quad(dth_dy, 0, b)
-
