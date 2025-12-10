12a6d3a6dc4550d58d57ac5ae0d7ff78e191bc5e
Jesse Misdorp <j.misdorp@student.tudelft.nl>
2025-12-10 17:25:21 +0100

    constants

--- WP5/Column_buckling.py
+++ WP5/Column_buckling.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+
+#---Constants---------------------------------------------------------------
+
+# --- Design 1 ---
+# Number of stringers per section (0-3m, 3-4.87m, 4.87-7m, 5-9.8m)
+Stringers_1_S1 = 6.0
+Stringers_1_S2 = 6.0
+Stringers_1_S3 = 3.0
+Stringers_1_S4 = 3.0
+
+A_1 = 45.0 # Area stringer (crossection) in cm^2
