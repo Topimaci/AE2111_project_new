0c1f0b36b1839eab71b7884a4c62478f1c4682eb
Otomars Gulbis <otomars.g@gmail.com>
2026-01-07 17:24:51 +0100

    weight code before parameters

--- WP5/weigth_calculation.py
+++ WP5/weigth_calculation.py
@@ -7,16 +7,16 @@ class weight_calculation:
         self.stringer_area = stringer_area
         self.skin_thickness = skin_thickness
         self.spar_thickness = spar_thickness
-        self.rib_thickness = 123
-        self.rib_lightening_coeff = 0.123 ## how much is metal
-        self.span = 123
+        self.rib_thickness = 123 ## modify
+        self.rib_lightening_coeff = 0.123 ## modify, how much is metal
+        self.span = 123 ## modify
         self.density = 3000 ## kg/m^3
-        self.c_root = 123
+        self.c_root = 123 ## modify
         self.wing_box_coordinates = [[0.6, 0.0781825],
                                      [0.1, 0.0781825],
                                      [0.1, -0.020718342983016523],
                                      [0.6, -0.026055198062595653]]
-        self.taper_ratio = 123
+        self.taper_ratio = 123 ## modify
         self.front_spar_h_frac = 0
         self.rear_spar_h_frac = 0
         self.top_stringers_c_frac = 0
@@ -73,6 +73,7 @@ class weight_calculation:
     def airfoil_area_unit_size(self):
         x = self.airfoil_coordinates[:, 0]
         y = self.airfoil_coordinates[:, 1]
+        ## Shoelace
         unit_area = 1/2*abs(np.dot(x, np.roll(y, -1)) - np.dot(y, np.roll(x, -1)))
         return unit_area
     
@@ -80,15 +81,47 @@ class weight_calculation:
         c_at_y = self.chord_length_at_y(y_pos)
         unit_area = self.airfoil_area_unit_size()
         area = c_at_y**2 * unit_area
-        weight = self.rib_lightening_coeff * self.rib_thickness * area
+        weight = self.rib_lightening_coeff * self.rib_thickness * area * self.density
         return weight
     
     def skin_weight(self):
-        
+        root_chord_coord_map = self.c_root * self.airfoil_coordinates
+        tip_chord_coord_map = self.taper_ratio * self.c_root * self.airfoil_coordinates
+        N = root_chord_coord_map.shape[0]
+        total_area = 0.0
+        for i in range(N-1):
+            quad = np.array([
+                root_chord_coord_map[i],
+                root_chord_coord_map[i+1],
+                tip_chord_coord_map[i+1],
+                tip_chord_coord_map[i]
+            ])
+            ## Shoelace
+            x = quad[:,0]
+            y = quad[:,1]
+            area = 0.5 * abs(np.dot(x, np.roll(y, -1)) - np.dot(y, np.roll(x, -1)))
+            total_area += area
+        weight = total_area * self.skin_thickness * self.density
+        return weight
+    
+    def wing_weight(self):
+        weight = 0.0
+        self.read_coordinate_file()
+        self.box_fractional_lengths()
+        weight += self.spar_weight("front")
+        weight += self.spar_weight("rear")
+        weight += self.stringers_weight("top")
+        weight += self.stringers_weight("bottom")
+        weight += self.rib_weight(self.span/(2 * 2))
+        weight += self.skin_weight()
+        return weight
+
+
 
 design_1 = weight_calculation(np.array([4, 4, 2, 2]),
                               np.array([4, 4, 2, 2]),
                               0.0001,
                               0.002,
                               0.005)
-design_1.box_fractional_lengths()
+
+print(design_1.wing_weight())
