43387b250ed2d7c8f5295923403f84e45bb75334
Benjamin Luneburg <benjaminluneburg@gmail.com>
2026-01-07 15:49:15 +0100

    changes

--- WP5/Column_buckling.py
+++ WP5/Column_buckling.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from compressionfailure import stress
+from Tension_failure import stress
 
 #---Constants---------------------------------------------------------------
 
