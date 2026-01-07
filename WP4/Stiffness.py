8d2636619071f2f64b4ee943f8267e63c3b21ed1
Bence <btomopulosz@tudelft.nl>
2026-01-07 14:23:18 +0100

    f

--- WP4/Stiffness.py
+++ WP4/Stiffness.py
@@ -146,8 +146,8 @@ q1, q2, dtheta = sp.symbols('q1 q2 dtheta')
 
 #_______TO BE REPLACED LATER__________________________________________
 y_breaks = np.array([0, 3, 4.89, 7]) #list of y-positions where the number of stringers decreases, stringer breaks as np.array([...])
-stringer_top_num = np.array([0, 0, 0, 0]) #nummber of stringer at the top per interval (that's why it's a list) in np.array([...])
-stringer_bottom_num = np.array([0, 0, 0, 0])  #nummber of stringer at the bottom per interval (that's why it's a list) in np.array([...])
+stringer_top_num = np.array([9, 9, 5, 5]) #nummber of stringer at the top per interval (that's why it's a list) in np.array([...])
+stringer_bottom_num = np.array([9, 9, 5, 5])  #nummber of stringer at the bottom per interval (that's why it's a list) in np.array([...])
 
 
 #Linear interpolation of the stringers
@@ -299,9 +299,9 @@ for i in range(len(x_grid)):
         results_geom["h_rs"][i],
         results_geom["c_upper"][i],
         results_geom["c_lower"][i],
-        0.002,
-        0.02,
-        0.00022,
+        0.003,
+        0.005,
+        0.0002,
         spar_list,
         G
     )
